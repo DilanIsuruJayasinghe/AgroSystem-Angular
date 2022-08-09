feaa49f534d85db870651df87546b585b75e905a
DilanIsuruJayasinghe <dilanisuru59@gmail.com>
2022-08-09 19:00:49 +0530

    Create Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Agro System - Angular';
-  name = 'Dilan Isuru Jayasinghe';
+  title = 'Agro - Agricultural System';
 }
